64b6a10ce9b189cc745cd1118f351d6d98e30276
fnsanches <felinobre100@gmail.com>
2022-12-28 19:25:49 -0300

    removing tests from address_parser.py file

--- address_parser.py
+++ address_parser.py
@@ -1,5 +1,4 @@
 import re
-import sys
 
 # Regex for number at start of string => ^[0-9]
 # Regex for strings that have -No 1000- => No\s\d+\D*$
@@ -31,22 +30,4 @@ def address_parser(string):
 
 
 if "__name__" == "__main__":
-    print(sys.argv[0])
-    t = 'Miritiba 339'
-    e = 'Babaçu 500'
-    s = 'Cambuí 804B'
-    te = '4, Rue de la République'
-
-    print(address_parser(t), address_parser(e),
-          address_parser(s), address_parser(te))
-
-    teste1 = 'Calle 44 No 1991'
-    teste = '1002 Street'
-    teste2 = 'Calle Sagasta, , , , 26'
-    print(address_parser(teste1))
-    print(address_parser(teste2))
-    print(address_parser(teste))
-
-    print(sys.argv[0])
-    print(sys.argv[1])
-    print(sys.argv[2])
+    address_parser()
